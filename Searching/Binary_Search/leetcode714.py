7b59b86e66b29093178ae8c3446a186cb5b0adc7
troykerim <troykerim@gmail.com>
2025-10-10 16:41:17 -0700

    minor fix

--- Searching/Binary_Search/leetcode714.py
+++ Searching/Binary_Search/leetcode714.py
@@ -5,7 +5,7 @@ You are given an array of distinct integers nums, sorted in ascending order, and
 
 Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
 
-Your solution must run in O(logn)O(logn) time.
+Your solution must run in O(logn) time.
 
 '''
 def search(nums, target):
