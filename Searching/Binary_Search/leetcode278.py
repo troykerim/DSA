33eb347260a88cbc15b19f93829dc3bfea9e0d5b
troykerim <troykerim@gmail.com>
2025-10-15 20:03:47 -0700

    Adding leetcode 278

---
+++ Searching/Binary_Search/leetcode278.py
@@ -0,0 +1,16 @@
+'''
+    Leetcode 278 First Bad Version 
+    
+    def isBadVersion is an API predifined in the website
+'''
+def firstBadVersion(n):
+    L = 1 
+    R = n 
+    
+    while L < R:
+        M = (L + R) // 2 
+        if (M):
+            R = M 
+        else:
+            L = M + 1
+    return L  
