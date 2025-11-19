e94cd939d3189d1eef0ed537eda5ba26ca491da9
troykerim <troykerim@gmail.com>
2025-11-19 14:43:28 -0800

    Adding leetcode 217

---
+++ Hash/leetcode217.py
@@ -0,0 +1,12 @@
+'''
+    Leetcode 217 - Contains Duplicate
+    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
