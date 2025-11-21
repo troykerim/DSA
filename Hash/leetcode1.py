d641ab08ce5e99a5a9541028e109cc5a5e19ec55
troykerim <troykerim@gmail.com>
2025-11-20 16:05:02 -0800

    Adding leetcode 1

---
+++ Hash/leetcode1.py
@@ -0,0 +1,15 @@
+'''
+    leetcode 1 - Two Sums
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val : index
+        for i, n in enumerate(nums):
+            diff = target - nums
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i 
+        return 
