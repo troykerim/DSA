06992a5a9f8c7f76488b36a1c2ae6341994f2c7e
troykerim <troykerim@gmail.com>
2026-02-12 13:33:20 -0800

    Adding leetcode 49

---
+++ Hash/leetcode49.py
@@ -0,0 +1,29 @@
+'''
+Leetcode 49 - Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+'''
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord("a")] += 1 
+            res[tuple(count)].append(s)
+        return res.values()
