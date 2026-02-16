b06b4ad9ddfb061f24ec6d2a6c0bbfad3199aacf
troykerim <troykerim@gmail.com>
2026-02-15 19:35:07 -0800

    adding leetcode 682

---
+++ Stack/baseballgame.py
@@ -0,0 +1,31 @@
+'''
+Leetcode 682 - Baseball game
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+    An integer x.
+        Record a new score of x.
+    '+'.
+        Record a new score that is the sum of the previous two scores.
+    'D'.
+        Record a new score that is the double of the previous score.
+    'C'.
+        Invalidate the previous score, removing it from the record.
+
+Return the sum of all the scores on the record after applying all the operations.
+'''
+class Solution(object):
+    def calPoints(self, operations):
+        stack = []
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "D":
+                stack.append(2* stack[-1])
+            elif op == "C":
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
+        
